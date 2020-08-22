b2f9f972f58f6798b3e53a8ec368dbe082b62965
Tushar Singh <tusharsingh432@gmail.com>
2020-08-22 19:52:35 +0530

    Menu I

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 function App() {
   return (
     <div className="App">
@@ -9,7 +10,8 @@ function App() {
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
-      </Navbar>  
+      </Navbar>
+      <Menu />
     </div>
   );
 }
